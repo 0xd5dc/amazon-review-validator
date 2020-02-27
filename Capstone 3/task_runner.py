30339e33140d815e19ad73c665e2f35da8d6a748
Ergo <summermick@gmail.com>
2020-02-26 20:26:15 -0800

    DRY Capstone 3/task_runner.py

--- Capstone 3/task_runner.py
+++ Capstone 3/task_runner.py
@@ -12,6 +12,23 @@ TOP_WORDS = 50000
 MAX_REVIEW_LENGTH = 600
 NUM_WORDS = 50000
 
+
+def split_pad(df):
+    X_train, X_test, Y_train, Y_test = train_test_split(df['review_body'].values, \
+                                                        df['label'].values, \
+                                                        test_size=0.30, \
+                                                        shuffle=True)
+    logging.info("Y train mean: {0}, Y test mean: {1}".format(Y_train.mean(), Y_test.mean()))
+    # tokenize the reviews text
+    tokenizer = Tokenizer(num_words=NUM_WORDS)
+    tokenizer.fit_on_texts(X_train)
+    X_train_tok = tokenizer.texts_to_sequences(X_train)
+    X_test_tok = tokenizer.texts_to_sequences(X_test)
+    X_train_pad = sequence.pad_sequences(X_train_tok, maxlen=MAX_REVIEW_LENGTH)
+    X_test_pad = sequence.pad_sequences(X_test_tok, maxlen=MAX_REVIEW_LENGTH)
+    return X_train_pad, X_test_pad, Y_train, Y_test
+
+
 if __name__ == '__main__':
     logging.basicConfig(filename='task.log', filemode='a', level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s')
@@ -28,18 +45,7 @@ if __name__ == '__main__':
     samples = pd.concat([samples, reviews[reviews['label'] == sorted_counts[0][0]]])
     logging.info(samples['label'].mean())
     # split data
-    X_train, X_test, Y_train, Y_test = train_test_split(samples['review_body'].values, \
-                                                        samples['label'].values, \
-                                                        test_size=0.30, \
-                                                        shuffle=True)
-    logging.info("Y train mean: {0}, Y test mean: {1}".format(Y_train.mean(), Y_test.mean()))
-    # tokenize the reviews text
-    tokenizer = Tokenizer(num_words=NUM_WORDS)
-    tokenizer.fit_on_texts(X_train)
-    X_train_tok = tokenizer.texts_to_sequences(X_train)
-    X_test_tok = tokenizer.texts_to_sequences(X_test)
-    X_train_pad = sequence.pad_sequences(X_train_tok, maxlen=MAX_REVIEW_LENGTH)
-    X_test_pad = sequence.pad_sequences(X_test_tok, maxlen=MAX_REVIEW_LENGTH)
+    X_train_pad, X_test_pad, Y_train, Y_test = split_pad(samples.sample(100))
 
     # compile models
     model = Sequential()
