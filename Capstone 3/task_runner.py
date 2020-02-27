376276cdb0961641e6b36f6494d7421e809e2ccf
Ergo <summermick@gmail.com>
2020-02-26 20:30:42 -0800

    DRY Capstone 3/task_runner.py

--- Capstone 3/task_runner.py
+++ Capstone 3/task_runner.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing import sequence
@@ -32,23 +34,17 @@ def split_pad(df):
 if __name__ == '__main__':
     logging.basicConfig(filename='task.log', filemode='a', level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s')
-    # load data
-    df = pd.read_csv(DATA_FILE, sep='\t')
-    # cast label to int
-    df['label'] = (df['verified_purchase'] == 'Y').astype(int)
-    reviews = df[['review_body', 'label']]
-    # count the label to find imbalanced value
-    counts = reviews.groupby("label")['label'].count().to_dict()
-    sorted_counts = sorted(counts.items(), key=lambda kv: kv[1])
-    # under-sampling
-    samples = reviews[reviews['label'] == sorted_counts[1][0]].sample(sorted_counts[0][1])
-    samples = pd.concat([samples, reviews[reviews['label'] == sorted_counts[0][0]]])
+    # load processed data
+    samples = pickle.load(open("verified_purchase_reviews.p", "rb"))
+    # log sample mean ideally to be .5
     logging.info(samples['label'].mean())
     # split data
     X_train_pad, X_test_pad, Y_train, Y_test = split_pad(samples.sample(100))
 
     # compile models
-    model = Sequential()
+    models = []
+    models.append(Sequential())
+    model = models[-1]
     model.add(Embedding(TOP_WORDS, EMBEDDING_VECTOR_LENGTH, input_length=MAX_REVIEW_LENGTH))
     # The output of SimpleRNN will be a 2D tensor of shape (batch_size, 128)
     model.add(SimpleRNN(128))
@@ -56,9 +52,10 @@ if __name__ == '__main__':
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # fit and eval models
-    # for model in models:
-    model.fit(X_train_pad, Y_train, epochs=10, batch_size=64, validation_data=(X_test_pad, Y_test))
-    # Final evaluation of the model on test data
-    scores = model.evaluate(X_test_pad, Y_test, verbose=0)
-    logging.info("Accuracy: %.2f%%" % (scores[1] * 100))
-    logging.info('done')
+    for n in [1000, 10000, 50000]:
+        X_train_pad, X_test_pad, Y_train, Y_test = split_pad(samples.sample(n))
+        for model in models:
+            model.fit(X_train_pad, Y_train, epochs=10, batch_size=64, validation_data=(X_test_pad, Y_test))
+            # Final evaluation of the model on test data
+            scores = model.evaluate(X_test_pad, Y_test, verbose=0)
+            logging.info("loss: {0:2.2f}, accuracy {1:2.2f}".format(scores[0], scores[1]))
